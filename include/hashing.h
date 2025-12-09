bf8cd29ce6c316671958be2c341194b247f963fe
wrozny <wrozny@gmail.com>
2025-12-09 23:19:26 +0100

    removed all the unimportant stuff, left only the Fovler-Noll-Vo a1 hash function for the future use in string based hashmaps

--- include/hashing.h
+++ include/hashing.h
@@ -19,30 +19,3 @@ static inline hash64 Hash_fnv_1a64(const char* bytes) {
 
     return hash;
 }
-
-/*
-    Generates 16 bytes of random characters between ASCII 33-125
-*/
-static inline char* Hash_NewRandomId() {
-    char* data = (char*)malloc(sizeof(char) * 17);
-
-    for(int i = 0; i < 16; i++) {
-        data[i] = (char) (33 + (rand() % 93));
-    }
-
-    data[16] = '\0';
-
-    return data;
-}
-
-
-/*
-    Returns fnv-1a hash for 2 integers of 64 bits
-*/
-static inline hash64 Hash_2intfnv_1a(int16_t a, int16_t b) {
-    char *buffer = malloc(sizeof(char) * 14);
-    sprintf_s(buffer, 14, "%d_%d", a, b);
-    hash64 hash = Hash_fnv_1a64(buffer);
-    free(buffer);
-    return hash;
-}
