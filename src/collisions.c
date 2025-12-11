9cf101c48143936a8bc7e356ee4ab72d60311bdc
wrozny <wrozny@gmail.com>
2025-12-11 22:06:13 +0100

    commented out the unused functions

--- src/collisions.c
+++ src/collisions.c
@@ -22,16 +22,16 @@ bool CollisionsRectsCollided(Rectangle rect1, Rectangle rect2) {
 /*
     Returns true or false whether game objects are inside eachother
 */
-bool CollisionsGameObjectsCollided(GameObject *a, GameObject *b) {
-    float aLeft = a->pos.x;
-    float aRight = a->pos.x + a->size.x;
-    float aTop = a->pos.y;
-    float aBottom = a->pos.y + a->size.y;
+bool CollisionsGameObjectsCollided(GameObject a, GameObject b) {
+    float aLeft = a.pos.x;
+    float aRight = a.pos.x + a.size.x;
+    float aTop = a.pos.y;
+    float aBottom = a.pos.y + a.size.y;
 
-    float bLeft = b->pos.x;
-    float bRight = b->pos.x + b->size.x;
-    float bTop = b->pos.y;
-    float bBottom = b->pos.y + b->size.y;
+    float bLeft = b.pos.x;
+    float bRight = b.pos.x + b.size.x;
+    float bTop = b.pos.y;
+    float bBottom = b.pos.y + b.size.y;
 
     return (aRight > bLeft && aLeft < bRight && aBottom > bTop && aTop < bBottom);
 }
@@ -41,53 +41,53 @@ bool CollisionsGameObjectsCollided(GameObject *a, GameObject *b) {
     Resolves collisions on physics object when collided with static object
 */
 void CollisionsResolvePhysicsAndStaticObjects(PhysicsObject *physicsObject, StaticObject *staticObject) {
-    if(!staticObject->canCollide) {
-        return;
-    }
+    // if(!staticObject->canCollide) {
+    //     return;
+    // }
 
-    GameObject *a = physicsObject->gameObject;
-    GameObject *b = staticObject->gameObject;
+    // GameObject a = physicsObject->gameObject;
+    // GameObject b = staticObject->gameObject;
 
-    float aLeft = a->pos.x;
-    float aRight = a->pos.x + a->size.x;
-    float aTop = a->pos.y;
-    float aBottom = a->pos.y + a->size.y;
+    // float aLeft = a.pos.x;
+    // float aRight = a.pos.x + a.size.x;
+    // float aTop = a.pos.y;
+    // float aBottom = a.pos.y + a.size.y;
 
-    float bLeft = b->pos.x;
-    float bRight = b->pos.x + b->size.x;
-    float bTop = b->pos.y;
-    float bBottom = b->pos.y + b->size.y;
+    // float bLeft = b.pos.x;
+    // float bRight = b.pos.x + b.size.x;
+    // float bTop = b.pos.y;
+    // float bBottom = b.pos.y + b.size.y;
 
-    if(aRight >= bLeft && aLeft <= bRight && aBottom >= bTop && aTop <= bBottom) {
-        float overlapLeft   = aRight - bLeft;
-        float overlapRight  = bRight - aLeft;
-        float overlapTop    = aBottom - bTop;
-        float overlapBottom = bBottom - aTop;
-        float minXOverlap = fminf(overlapLeft, overlapRight);
-        float minYOverlap = fminf(overlapTop, overlapBottom);
+    // if(aRight >= bLeft && aLeft <= bRight && aBottom >= bTop && aTop <= bBottom) {
+    //     float overlapLeft   = aRight - bLeft;
+    //     float overlapRight  = bRight - aLeft;
+    //     float overlapTop    = aBottom - bTop;
+    //     float overlapBottom = bBottom - aTop;
+    //     float minXOverlap = fminf(overlapLeft, overlapRight);
+    //     float minYOverlap = fminf(overlapTop, overlapBottom);
 
-        // Resolve using the axis with *less* penetration
-        if (minXOverlap <= minYOverlap) {
-            //Resolve horizontally
-            if (overlapLeft < overlapRight) {
-                a->pos.x -= overlapLeft;
-            } else {
-                a->pos.x += overlapRight;
-            }
+    //     // Resolve using the axis with *less* penetration
+    //     if (minXOverlap <= minYOverlap) {
+    //         //Resolve horizontally
+    //         if (overlapLeft < overlapRight) {
+    //             a.pos.x -= overlapLeft;
+    //         } else {
+    //             a.pos.x += overlapRight;
+    //         }
            
-            physicsObject->velocity.x = 0.0f;
-        } else {
-            //Resolve vertically
-            physicsObject->inAir = false;
+    //         physicsObject->velocity.x = 0.0f;
+    //     } else {
+    //         //Resolve vertically
+    //         physicsObject->inAir = false;
 
-            if (overlapTop < overlapBottom) {
-                a->pos.y -= overlapTop;
-            } else {
-                a->pos.y += overlapBottom;
-            }
+    //         if (overlapTop < overlapBottom) {
+    //             a.pos.y -= overlapTop;
+    //         } else {
+    //             a.pos.y += overlapBottom;
+    //         }
 
-            physicsObject->velocity.y = 0.0f;
-        }
-    }
+    //         physicsObject->velocity.y = 0.0f;
+    //     }
+    // }
 }
 
