dd04d3e7a3bfb771ec6fa92f8fd74e3dcab19121
wrozny <wrozny@gmail.com>
2025-12-11 22:05:33 +0100

    removed physics calculation logic

--- src/game.c
+++ src/game.c
@@ -18,39 +18,18 @@ static inline float clampf(float value, float min, float max) {
 
 void PhysicsUpdate(double deltaTime) {
     Vector2 inputDirection = InputGetDirection();
-    // inputDirection.x = inputDirection.x * PLAYER_SPEED * (float)deltaTime;
-    // inputDirection.y = -inputDirection.y * PLAYER_SPEED * (float)deltaTime;
+    inputDirection.x = inputDirection.x * PLAYER_SPEED * (float)deltaTime;
+    inputDirection.y = -inputDirection.y * PLAYER_SPEED * (float)deltaTime;
 
-    float horizontalVelocity = inputDirection.x * PLAYER_SPEED * PLAYER_SPEED * (float)deltaTime;
-    float verticalVelocity = 0.0f;
+    PlayerCamera.target = Vector2Add(inputDirection, PlayerCamera.target);
 
-    if(!PlayerPhysicsObject->inAir) {
-        verticalVelocity = -inputDirection.y * PLAYER_SPEED * 2.92f;
-    }
-
-
-    Vector2 newVelocity = 
-        (Vector2) {
-            clampf(PlayerPhysicsObject->velocity.x + horizontalVelocity, -PLAYER_SPEED * 1.5f, PLAYER_SPEED * 1.5f), 
-            clampf(PlayerPhysicsObject->velocity.y + verticalVelocity, -PLAYER_SPEED * 5.0f, PLAYER_SPEED * 5.0f)
-        };
-
-    PlayerPhysicsObject->velocity = newVelocity;
-   
-    PlayerCamera.target = Vector2Subtract(PlayerPhysicsObject->gameObject->pos, (Vector2) {SCREEN_WIDTH / 2.0f, SCREEN_HEIGHT / 2.0f});
-    SceneUpdatePhysics(currentScene, deltaTime);
-
-    if(IsKeyDown(KEY_R)) {
-        PlayerPhysicsObject->velocity = Vector2Zero();
-        PlayerPhysicsObject->gameObject->pos = (Vector2) {100, -200};
-    }
+    //printf("%.3f\n", 1 / GetFrameTime());
 }
 
 void OnFrameRender(double deltaTime) {
     BeginMode2D(PlayerCamera);
     SceneDraw(currentScene);
     EndMode2D();
-    AssetsDrawSprite(SI_ASHTAR_SHERAN_ICON, (Rectangle) {.x = 0, .y = 360, .height = 360, .width = 240}, 0.0f);
 }
 
 
@@ -61,17 +40,6 @@ bool InitGame() {
     LoadMapToScene(currentScene, MAPTYPE_AGARTHA_START);
     InitPlayer();
     SetTargetFPS(60);
-
-    SceneAddPhysicsObject(currentScene, PlayerPhysicsObject);
-
-    PhysicsObject *sheran1 = (PhysicsObject*)vec_get(currentScene->physicsObjects, 0);
-    sheran1->velocity = (Vector2) {500, -500};
-
-
-    PhysicsObject *sheran2 = PhysicsObjectCreate(SI_ASHTAR_SHERAN_ICON, (Vector2){0, 100}, (Vector2){32, 32}, 0.0f);
-    sheran2->velocity = (Vector2){800, -450};
-
-    SceneAddPhysicsObject(currentScene, sheran2);
 
     if (!IsWindowReady()) {
         return false;
