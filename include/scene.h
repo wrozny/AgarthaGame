9776e5971ad5a180ad4724422a2a276d58d15738
wrozny <wrozny@gmail.com>
2025-12-14 16:30:26 +0100

    added new chunk managment functions, SceneObjectGroupBuffer for finding chunks in a radius and chunk rendering

--- include/scene.h
+++ include/scene.h
@@ -5,6 +5,9 @@
 #include "physics/physics.h"
 #include "hashing.h"
 
+extern ObjectGroup_t **SceneObjectGroupBuffer;
+
+#define CHUNK_PIXEL_SIZE 256
 
 
 typedef struct Scene {
@@ -29,6 +32,11 @@ void SceneDestroy(Scene* scene);
 void SceneDraw(Scene* scene);
 
 /*
+    Draws the scene with debug data
+*/
+void SceneDrawDebug(Scene* scene);
+
+/*
     Calculates physics tick for every physics object in the scene
 */
 void SceneUpdatePhysics(Scene* scene, double deltaTime);
@@ -42,3 +50,24 @@ void SceneAddStaticObject(Scene* targetScene, StaticObject* staticObject);
     Adds new object with physics applied to scene
 */
 void SceneAddPhysicsObject(Scene* targetScene, PhysicsObject* physicsObject);
+
+
+/*
+    Packs two int16's into one uint32
+*/
+static inline uint32_t ScenePackInt16(int16_t a, int16_t b) {
+    return ((uint32_t)a << 16) | b;
+}
+
+/*
+    Returns the uint32 id of a chunk based on CHUNK_PIXEL_SIZE
+*/
+static inline uint32_t SceneGetChunkId(float x, float y) {
+    return ScenePackInt16((int16_t)(x / CHUNK_PIXEL_SIZE), (int16_t)(y / CHUNK_PIXEL_SIZE));
+}
+
+/*
+    Given the target rectangle writes to SceneObjectGroupBuffer every ObjectGroup pointer that target is in 
+    useful both in physics simulation and chunk rendering
+*/
+void SceneUpdateObjectGroupBuffer(Scene *scene, Rectangle target);
--- src/scene.c
+++ src/scene.c
@@ -1,12 +1,38 @@
 #include "scene.h"
-
 #include "ds/stb_ds.h"
+
+#include "player.h"
+
+ObjectGroup_t **SceneObjectGroupBuffer;
+
+/*
+    Returns chunk if exists, otherwise creates one
+*/
+static inline ObjectGroup_t* SceneEnsureChunk_f(Scene* targetScene, uint32_t chunkId) {
+    ObjectGroup_t *chunk = hmget(targetScene->chunks, chunkId);
+
+    if(!chunk) {
+        ObjectGroup_t *newChunk = ObjectGroupCreate();
+        hmput(targetScene->chunks, chunkId, newChunk);
+        chunk = hmget(targetScene->chunks, chunkId);
+    }
+
+    return chunk;
+}
+
+/*
+    Returns pointer to chunk if exists, otherwise NULL
+*/
+static inline ObjectGroup_t* SceneGetChunk_f(Scene* targetScene, uint32_t chunkId) {
+    return hmget(targetScene->chunks, chunkId);
+}
 
 Scene* SceneCreate() {
     Scene* newScene = (Scene*)malloc(sizeof(Scene));
     newScene->allObjects = ObjectGroupCreate();
     newScene->chunks = NULL;
     newScene->gravity = 9.81f;
+    
     return newScene;
 }
 
@@ -17,8 +43,68 @@ void SceneDestroy(Scene* scene) {
 }
 
 void SceneDraw(Scene* scene) {
-   ObjectGroupDraw(scene->allObjects);
+    SceneUpdateObjectGroupBuffer(scene, 
+        (Rectangle) {
+            .x = PlayerCamera.target.x, 
+            .y = PlayerCamera.target.y, 
+            .width = 960.0f / PlayerCamera.zoom, 
+            .height = 720.0f / PlayerCamera.zoom
+        }
+    );
+
+    ObjectGroup_t **chunkLookupPtr = SceneObjectGroupBuffer;
+    
+    for(; *chunkLookupPtr; ++chunkLookupPtr) {
+        ObjectGroup_t *currentChunk = *chunkLookupPtr;
+        ObjectGroupDraw(currentChunk);
+    }
 }
+
+/*
+    Draws the scene with debug data
+*/
+void SceneDrawDebug(Scene* scene) {
+    SceneUpdateObjectGroupBuffer(scene, 
+        (Rectangle) {
+            .x = PlayerCamera.target.x, 
+            .y = PlayerCamera.target.y, 
+            .width = 960.0f / PlayerCamera.zoom, 
+            .height = 720.0f / PlayerCamera.zoom
+        }
+    );
+
+    ObjectGroup_t **chunkLookupPtr = SceneObjectGroupBuffer;
+    int chunks = 0;
+
+    for(; *chunkLookupPtr; ++chunkLookupPtr) {
+        ObjectGroup_t *currentChunk = *chunkLookupPtr;
+        StaticObject *anyStaticObject = currentChunk->staticObjects[0].value;
+
+        int chunkX;
+        int chunkY;
+
+        if(anyStaticObject) {
+            chunkX = ((int)(anyStaticObject->gameObject.pos.x) / CHUNK_PIXEL_SIZE) * CHUNK_PIXEL_SIZE;
+            chunkY = ((int)(anyStaticObject->gameObject.pos.y) / CHUNK_PIXEL_SIZE) * CHUNK_PIXEL_SIZE;
+        }
+
+        DrawRectangleLines(chunkX, chunkY, CHUNK_PIXEL_SIZE, CHUNK_PIXEL_SIZE, WHITE);
+        ObjectGroupDraw(currentChunk);
+        chunks++;
+    }
+
+    char buffer[64];
+
+    #ifdef WIN32
+        sprintf_s(buffer, 64, "chunks = %d", chunks);
+    #else
+        sprintf(buffer, "chunks = %d", chunks);
+    #endif
+
+    
+    DrawText(buffer, (int)PlayerCamera.target.x, (int)PlayerCamera.target.y, 20, WHITE);
+}
+
 
 /*
     Calculates physics tick for every physics object in the scene
@@ -32,6 +118,9 @@ void SceneUpdatePhysics(Scene* scene, double deltaTime) {
 */
 void SceneAddStaticObject(Scene* targetScene, StaticObject *staticObject) {
     ObjectGroupSet(targetScene->allObjects, staticObject);
+    uint32_t chunkId = SceneGetChunkId(staticObject->gameObject.pos.x, staticObject->gameObject.pos.y);
+    ObjectGroup_t* chunk = SceneEnsureChunk_f(targetScene, chunkId);
+    ObjectGroupSet(chunk, staticObject);
 }
 
 /*
@@ -39,4 +128,37 @@ void SceneAddStaticObject(Scene* targetScene, StaticObject *staticObject) {
 */
 void SceneAddPhysicsObject(Scene* targetScene, PhysicsObject *physicsObject) {
     ObjectGroupSet(targetScene->allObjects, physicsObject);
+    uint32_t chunkId = SceneGetChunkId(physicsObject->gameObject.pos.x, physicsObject->gameObject.pos.y);
+    ObjectGroup_t* chunk = SceneEnsureChunk_f(targetScene, chunkId);
+    ObjectGroupSet(chunk, physicsObject);
+}
+
+/*
+    Given the target rectangle writes to SceneObjectGroupBuffer every ObjectGroup pointer that target is in 
+    useful both in physics simulation and chunk rendering
+*/
+void SceneUpdateObjectGroupBuffer(Scene *scene, Rectangle target) {
+    int16_t minChunkX = (int16_t)target.x / CHUNK_PIXEL_SIZE;
+    int16_t maxChunkX = (int16_t)(target.x + target.width) / CHUNK_PIXEL_SIZE;
+
+    int16_t minChunkY = (int16_t)target.y / CHUNK_PIXEL_SIZE;
+    int16_t maxChunkY = (int16_t)(target.y + target.height) / CHUNK_PIXEL_SIZE;
+
+    int freeIdx = 0;
+    
+    for (int16_t currChunkX = minChunkX; currChunkX <= maxChunkX; currChunkX++) {
+        for (int16_t currChunkY = minChunkY; currChunkY <= maxChunkY; currChunkY++) {
+            //DrawRectangleLines();
+
+            uint32_t chunkId = ScenePackInt16(currChunkX, currChunkY);
+            ObjectGroup_t *objectGroup = SceneGetChunk_f(scene, chunkId);
+
+            if (objectGroup) {
+                SceneObjectGroupBuffer[freeIdx] = objectGroup;
+                freeIdx++;
+            }
+        }
+    }
+
+    SceneObjectGroupBuffer[freeIdx] = NULL;
 }
